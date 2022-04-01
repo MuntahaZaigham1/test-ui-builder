aa1a2c87e7c261289aa0e2ff5f2c8890d549812a
Muntaha Zaigham <l164387@lhr.nu.edu.pk>
2022-04-01 18:15:26 +0500

    button widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test-project-ui-builder';
+  field:any;
+  form: any;
+  constructor(    private fb: FormBuilder    ){
+    this.field= {
+      id: 1648564184459,
+      type: "button",
+      name: "button",
+      // inputType: "button",
+      label: "sdsdsd",
+      insideStep: false,
+      fxFlexAlign: "auto",
+      // formField: true,
+      matFormFieldColor: "accent",
+      // from:"rightSideBar",
+      customName: "button1648564009814",
+      customId: 1648564009814,
+      title:"hello",
+      btnDirective: "mat-raised-button",
+      iconTypeIconButton: "fontLigature",
+      parentId: 1648564182543,
+      columnIndex: 0,
+      onClick: { functionCB: this.fun, parameters: [] }
+    }
+    const group = this.fb.group({});
+    // group.addControl(this.field.formControl, this.fb.control(this.field.value));
+    this.form = group;
+  }
+  ngOnInit(): void {
+      
+  }
+  fun(event:any) {
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxDnDModule } from '@swimlane/ngx-dnd';
+import { AgGridModule } from 'ag-grid-angular';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { LoadComponentDirective, UiBuilder } from 'ui-builder-library';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadComponentDirective.LoadComponentDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxDnDModule.forRoot(),
+    ColorPickerModule,
+    AgGridModule,
+    UiBuilder.AppModule,
   ],
   providers: [],
+  exports:[ LoadComponentDirective.LoadComponentDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
